a9493a56f1844bb165f7dde55966803fc1856089
Khushi Tamor <tamorkhushi@gmail.com>
2026-08-23 14:56:18 +0530

    create routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
 
+import productRoutes from "./routes/productRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config();
 
@@ -25,10 +27,11 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
